b0d6b7172c5caa90a2b5f1229d4a1a976ef60498
cxz <pink.tortoiser@gmail.com>
2020-03-26 17:41:42 +0800

    compute_distance between triangle and point

---
+++ build/CMakeFiles/MAIN.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/lonovo/Desktop/CAD/3.17/src/compute_distance.cpp" "/mnt/c/Users/lonovo/Desktop/CAD/3.17/build/CMakeFiles/MAIN.dir/src/compute_distance.cpp.o"
+  "/mnt/c/Users/lonovo/Desktop/CAD/3.17/src/main.cpp" "/mnt/c/Users/lonovo/Desktop/CAD/3.17/build/CMakeFiles/MAIN.dir/src/main.cpp.o"
+  "/mnt/c/Users/lonovo/Desktop/CAD/3.17/src/operator.cpp" "/mnt/c/Users/lonovo/Desktop/CAD/3.17/build/CMakeFiles/MAIN.dir/src/operator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MAIN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MAIN.dir/src/compute_distance.cpp.o"
+  "CMakeFiles/MAIN.dir/src/main.cpp.o"
+  "CMakeFiles/MAIN.dir/src/operator.cpp.o"
+  "../bin/MAIN.pdb"
+  "../bin/MAIN"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MAIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
